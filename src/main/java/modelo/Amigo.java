61a0249b75179c739f6aa6fc8264760655764c1a
Victor Oliveira <victorallstaromv@gmail.com>
2024-11-13 19:38:26 -0300

    adicionei id, com get e set.

--- src/main/java/modelo/Amigo.java
+++ src/main/java/modelo/Amigo.java
@@ -1,18 +1,26 @@
 package modelo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-/**
- *
- * @author joaov
- */
 public class Amigo {
+    private int id;
     private String nome;
     private String telefone;
 
-    public Amigo(String nome, String telefone) {
+    public Amigo(int id, String nome, String telefone) {
+        this.id = id;
         this.nome = nome;
         this.telefone = telefone;
     }
-
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
     public String getNome() {
         return nome;
     }
