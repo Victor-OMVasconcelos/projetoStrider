f2bb8e02aa0d6814857d3ae3431d30f4d684d127
anaca <anaca@10.119.164.81>
2024-11-19 19:33:27 -0300

    Modd: Código morto removido

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,4 +1,5 @@
 package principal;
+
 import modelo.Amigo;
 import visao.FrmMenuPrincipal;
 import java.sql.SQLException;
@@ -10,19 +11,10 @@ import java.sql.SQLException;
 public class Principal {
 
     public static void main(String[] args) {
-        /*
-        try {
-            Amigo amigo = new Amigo(3, "cauan", "8856-789");
-            amigo.save();
-            System.out.println("Dados salvos");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        */
-        
+
         FrmMenuPrincipal objeto = new FrmMenuPrincipal();
         objeto.setLocationRelativeTo(null);
         objeto.setVisible(true);
-       
+
     }
 }
