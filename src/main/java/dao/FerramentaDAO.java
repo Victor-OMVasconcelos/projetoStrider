12bc686c5e8faf883e9878e93bed0cbb27ed9687
Victor Oliveira <victorallstaromv@gmail.com>
2024-11-20 20:54:22 -0300

    refactor: removido todo o codigo de getconexao

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -58,40 +58,7 @@ public class FerramentaDAO {
         return maiorID;
     }
 
-    //Retorna conexão com o banco de dados
-    public Connection getConexao() {
-
-        Connection connection = null;  //instância da conexão
-        try {
-            // Carregamento do JDBC Driver
-            String driver = "com.mysql.cj.jdbc.Driver";
-            Class.forName(driver);
-
-            // Configurar a conexão
-            String server = "localhost"; //caminho do MySQL
-            String database = "strider_db";
-            String url = "jdbc:mysql://localhost:3306/strider_db";
-            String user = "root";
-            String password = "root";
-
-            connection = DriverManager.getConnection(url, user, password);
-            // Testando..
-            if (connection != null) {
-                System.out.println("Status: Conectado!");
-            } else {
-                System.out.println("Status: NÃO CONECTADO!");
-            }
-            return connection;
-
-        } catch (ClassNotFoundException e) {  //Driver não encontrado
-            System.out.println("O driver nao foi encontrado. " + e.getMessage());
-            return null;
-        } catch (SQLException e) {
-            System.out.println("Nao foi possivel conectar...");
-            return null;
-        }
-    }
-
+    
     //Insere Ferramenta na db
     public boolean insereFerramentaBD(Ferramenta objeto) {
         String sql = "INSERT INTO tb_ferramentas(id,nome,marca,preco) VALUES(?,?,?,?)";
