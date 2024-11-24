10c9551138e1414878689e0f74683ec04d5e7bcc
Victor Oliveira <victorallstaromv@gmail.com>
2024-11-24 19:20:51 -0300

    add: classe FerramentaDAO

---
+++ src/main/java/dao/EmprestimoDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import modelo.Emprestimo;
+
+/**
+ *
+ * @author Victor Omv
+ */
+public class EmprestimoDAO {
+    
+}
