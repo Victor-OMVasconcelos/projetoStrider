8aa863690be7f91dbd05138aca76de3116eef255
JoaoFerraz <camargo.ferraz.joao.vitor@gmail.com>
2024-11-24 00:57:35 -0300

    style: Alinha o código às normas de codificação e padronização.

--- src/main/java/dao/ConexaoBd.java
+++ src/main/java/dao/ConexaoBd.java
@@ -1,14 +1,30 @@
 package dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Classe responsável por estabelecer a conexão com o banco de dados do mySQL.
+ */
 public class ConexaoBd {
+
+    // URL de conexão com o banco de dados
     private static final String URL = "jdbc:mysql://localhost:3306/strider_db";
-    private static final String USER = "user_remoto";
-    private static final String PASSWORD = "acesso_remoto_jocavi";
-    
+
+    // Usuário para autenticação no banco de dados
+    private static final String USER = "root"; // Corrigido a sintaxe do usuário
+
+    // Senha para autenticação no banco de dados
+    private static final String PASSWORD = "root";
+
+    /**
+     * Estabelece a conexão com o banco de dados.
+     * @return Uma instância de Connection com a conexão aberta
+     * @throws SQLException Se ocorrer um erro ao estabelecer a conexão
+     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
